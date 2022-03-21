fc6220c7782ed6237b5a0e03f17de67cb07bea96
Juliana Samoylenko <yuliana.samoilenko@flatstack.com>
2022-03-22 00:09:27 +0300

    use new components on home page

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
 // import { Link } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
-import { useApolloClient } from '@apollo/client';
 
 import useAuthUser from 'globals/AuthUser';
 // import createProject from 'api/mutations/createProject';
@@ -9,6 +8,8 @@ import useCreateProject from 'hooks/mutations/useCreateProject';
 import useCurrentUser from 'hooks/query/useCurrentUser';
 
 import DefaultLayout from 'components/Layouts/DefaultLayout/DefaultLayout';
+import EntityListWrapper from 'components/EntityListWrapper/EntityListWrapper';
+import EntityCard from 'components/EntityCard';
 import Button from 'components/form/inputs/Button';
 
 const INITIAL_FORM_STATE = {
@@ -17,15 +18,14 @@ const INITIAL_FORM_STATE = {
 };
 
 export default function Index() {
-	const { /* dispatch, */ state: AuthUserState } = useAuthUser();
+	const { dispatch, state: AuthUserState } = useAuthUser();
 	const navigate = useNavigate();
-	// const handleClick = () => {
-	// 	dispatch({ type: 'logout' });
-	// 	navigate('/login');
-	// };
+	const handleLogoutClick = () => {
+		dispatch({ type: 'logout' });
+		navigate('/login');
+	};
 	const [formState, setFormState] = useState(INITIAL_FORM_STATE);
 
-	// const client = useApolloClient();
 	const { create } = useCreateProject();
 	const { currentUser } = useCurrentUser();
 
@@ -44,14 +44,14 @@ export default function Index() {
 	return (
 		<DefaultLayout title="Home page">
 			{/* <Link to={'/login'}> Go to login page </Link>
-			<Link to={'/registration'}> Go to registration page </Link>
-			<Button onClick={handleClick}> Logout </Button> */}
+			<Link to={'/registration'}> Go to registration page </Link> */}
+			<Button onClick={handleLogoutClick}>Logout</Button>
 			<Button onClick={handleClick}>Create project</Button>
-			<ul>
+			<EntityListWrapper>
 				{currentUser?.projects?.map((project) => (
-					<li key={project.id}>{project.name}</li>
+					<EntityCard key={project.id} entityName="Project" name={project.name} description={project.description} />
 				))}
-			</ul>
+			</EntityListWrapper>
 		</DefaultLayout>
 	);
 }
