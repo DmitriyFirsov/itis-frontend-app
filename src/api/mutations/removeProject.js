e2694ced3a2b660eb71d3b58284c9a28e20f6b85
DmitriyFirsov <dmitriy.firsov@flatstack.com>
2022-03-25 23:25:02 +0500

    fix removing progect

--- src/api/mutations/removeProject.js
+++ src/api/mutations/removeProject.js
@@ -1,7 +1,7 @@
 import { gql } from '@apollo/client';
 
 export default gql`
-	mutation destroyProject($projectId: ID!) {
-		destroyProject(projectId: $projectId)
+	mutation destroyProject($id: ID!) {
+		destroyProject(projectId: $id)
 	}
 `;
